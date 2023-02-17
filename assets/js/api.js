4f74e6b741faa3845e2e4e7e117aa1baa1b7f283
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-17 01:21:23 +0100

    the api error was properly fixed.

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,19 +6,19 @@ fetch('https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATES
     // console.log(datas.find(countries => countries.country === 'Nigeria'));
     const getIn = datas.find(countries => countries.country === 'Nigeria')
     console.log(getIn)
-    const { infected, recovered, deceased, activeCases } = getIn;
-    let add = infected + recovered + deceased + activeCases;
-    const numOfActiveCases = activeCases;
+    const { infected, recovered, deceased } = getIn;
+    let add = infected + recovered + deceased;
+    const infectedCases = infected;
     const numRecoveredPeople = recovered;
     const numDeceasedPeople = deceased;
     const totalActiveCases = document.getElementById('active');
-    totalActiveCases.innerHTML = `${numOfActiveCases}`;
+    totalActiveCases.innerHTML = `${infectedCases}`;
     const totalCases = document.getElementById('total');
+    totalCases.innerHTML = `${add}`;
     const recoveredCases = document.getElementById('recovered');
     recoveredCases.innerHTML = `${numRecoveredPeople}`;
     const deathCases = document.getElementById('deaths');
     deathCases.innerHTML = `${numDeceasedPeople}`;
-    totalCases.innerHTML = `${add}`;
 
 
 
